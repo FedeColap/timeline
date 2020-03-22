01c514461a53024ac84c5e787e6c13a278b426c6
Federica Colapaoli <federica.colapaoli@coresecure.com>
2020-03-22 16:09:49 -0400

    progress bar adjourned

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,65 +1,56 @@
 import * as React from 'react';
 import './timeline.css';
-// export interface ITimelineProps {
-//   children?: React.ReactNode;
-//   // status: OrderStatus
-// }
-
-// const Timeline: React.SFC<ITimelineProps> = (props) => (
-
-//     <div className="timeline">
-//         <div className="timeline-progress"></div>
-//         <div className="timeline-items">
-//               <div className="timeline-item active">
-//                   <div className="timeline-content">
-//                     Step 1
-//                   </div>
-//               </div>
-//               <div className="timeline-item active">
-//                   <div className="timeline-content">
-//                     Step 2
-//                   </div>
-//               </div>
-//               <div className="timeline-item">
-//                   <div className="timeline-content">
-//                     Step 3
-//                   </div>
-//               </div>
-//               <div className="timeline-item">
-//                   <div className="timeline-content">
-//                     Step 4
-//                   </div>
-//               </div>
-//         </div>
-//     </div>
-// );
-
-// export enum OrderStatus {
-//   Open ="Open", 
-//   Accepted ="Accepted by Vendor",
-//   Progress ="Order in Progress",
-//   Completed ="Completed"
-// }
-// Timeline.defaultProps = {
-//   children: null
-// };
-
-// export default Timeline;
 
 export interface TimelineProps {
   text: string, 
   active?: boolean,
-  status: Array<any>
+  status: Array<any>, 
+  currentStatus: string,
+  barLenght: number
 }
 
  
 const Timeline: React.SFC<TimelineProps> = (props) => {
 
   const totalItems = props.status.length;
-	const numberOfActiveItems = props.status.filter(item => item.active).length;
+  const numberOfActiveItems = props.barLenght + 1;
 	const progressBarWidth = totalItems > 1 ? (numberOfActiveItems - 1) / (totalItems - 1) * 100 : 0;
 
   return (
+    <div className="wrapper">
+      <div className="timeline">
+          <div className="timeline-progress" style={{ width: `${progressBarWidth}%`}}></div>
+          <div className="timeline-items">
+                <div className="timeline-item active">
+                    <div className="timeline-content">
+                      {props.status[0]}
+                    </div>
+                </div>
+                <div className="timeline-item active">
+                    <div className="timeline-content">
+                      {props.status[1]}
+                    </div>
+                </div>
+                <div className="timeline-item">
+                    <div className="timeline-content">
+                      {props.status[2]}
+                    </div>
+                </div>
+                <div className="timeline-item">
+                    <div className="timeline-content">
+                      {props.status[3]} 
+                    </div>
+                </div>
+          </div>
+      </div>
+      <div className="status-report">
+        <p className="status-report-content">Your closing is currently in:</p> <h2 className="status-report-current">{props.currentStatus}</h2>
+      </div>
+    </div>
+
+    //I have not used the .map() because at the moment I don't know how to indicate the "active" status
+    //without turning the enum in a more complex object
+    
     // <div className="timeline">
 		// 	<div className="timeline-progress"></div>
 		// 	<div className="timeline-items">
@@ -72,33 +63,6 @@ const Timeline: React.SFC<TimelineProps> = (props) => {
 		// 		))}
 		// 	</div>
     // </div>
-    
-
-    <div className="timeline">
-        <div className="timeline-progress" style={{ width: `${progressBarWidth}%`}}></div>
-        <div className="timeline-items">
-              <div className="timeline-item active">
-                  <div className="timeline-content">
-                    {props.status[0]}
-                  </div>
-              </div>
-              <div className="timeline-item active">
-                  <div className="timeline-content">
-                    {props.status[1]}
-                  </div>
-              </div>
-              <div className="timeline-item active">
-                  <div className="timeline-content">
-                    {props.status[2]}
-                  </div>
-              </div>
-              <div className="timeline-item">
-                  <div className="timeline-content">
-                    {props.status[3]} 
-                  </div>
-              </div>
-        </div>
-    </div>
   );
 }
  
--- src/components/timeline.stories.tsx
+++ src/components/timeline.stories.tsx
@@ -3,9 +3,7 @@ import * as React from 'react';
 import Timeline from "./Timeline";
 storiesOf("Timeline", module)
   .add("default", () => (
-    <Timeline text={"Orders"} status={...statuses}></Timeline>
-    // <Timeline 
-    // />
+    <Timeline text={"Orders"} status={...statuses} currentStatus={currentStatus} barLenght={barLenght}></Timeline>
   ))
 
   export enum OrderStatus {
@@ -14,5 +12,19 @@ storiesOf("Timeline", module)
     Progress ="Order in Progress",
     Completed ="Completed"
   }
+  console.log(OrderStatus)
   const statuses = Object.values(OrderStatus)
   console.log(statuses)
+
+
+  let currentStatus = statuses[0];
+  let barLenght= statuses.indexOf(currentStatus)
+  console.log(barLenght)
+
+  //check this condition to see the "updates of states"
+  if(3 > 2){
+    barLenght +=1;
+    console.log(barLenght)
+    currentStatus= statuses[barLenght]
+  }
+
